4533f4884c7efc7f50ed97efc3bc8143a68c7dfa
Carlos Henrique Trindade <119907560+carloshenrique44@users.noreply.github.com>
2024-03-11 18:32:54 -0300

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,30 @@
+import tkinter as tk   
+
+root = tk.Tk()
+
+root.geometry('800x800')
+root.title('Contador de click US guri')
+root.configure(bg='#1d1d1d')
+
+numero = 0
+
+def aumentar():
+    global numero
+    numero = numero + 1
+    contagem_click.configure(text=numero)
+def diminuir():
+    global numero
+    numero = numero - 1
+    contagem_click.configure(text=numero)
+
+
+margem = tk.Canvas(root, height=300, bg='#1d1d1d', bd=0, highlightthickness=0, relief='flat')
+margem.pack()
+botao_aumentar = tk.Button(root, bg='#FFFFFF', text='+', font=('Montserrat', 16, 'bold'), padx=10, command=aumentar)
+botao_aumentar.pack()
+contagem_click = tk.Label(root, text=numero, fg='#FFFFFF', bg='#1d1d1d', font=('Montserrat', 16, 'bold'))
+contagem_click.pack()
+botao_diminuir = tk.Button(root, bg='#FFFFFF', text='-', font=('Montserrat', 16, 'bold'), padx=10, command=diminuir)
+botao_diminuir.pack()
+
+root.mainloop()
